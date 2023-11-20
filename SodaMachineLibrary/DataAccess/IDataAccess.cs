d1ff0c45e807552f846f5e44d82f1bbb25115b1b
nakar <nakar@LAPTOP-7J4ALMQ1>
2023-11-20 16:25:50 -0500

    Add project files.

---
+++ SodaMachineLibrary/DataAccess/IDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using SodaMachineLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachineLibrary.DataAccess
+{
+    public interface IDataAccess
+    {
+        List<SodaModel> SodaInventoryGetTypes();
+        SodaModel SodaInventory_GetSoda(SodaModel soda);
+        void SodaInventory_AddSodas(List<SodaModel> sodas);
+        List<SodaModel> SodaInventory_GetAll();
+        void UserCredit_Insert(string UserId, decimal amount);
+        void UserCredit_Clear(string UserId);
+        decimal UserCredir_Total(string UserId);
+        void UserCredit_Deposit(string UserId);
+        decimal MachineInfo_SadaPrice();
+        decimal MachineInfo_EmptyCash();
+        decimal MachineInfo_CashOnHand();
+        decimal MachineInfo_TotalIncome();
+        List<CoinModel> CoinInventory_WithdrawCoins(decimal CoinValue, int quantity);
+        List<CoinModel> CoinInventory_GetAll();
+        void CoinInventory_AddCoins(List<CoinModel> coins);
+
+
+
+    }
+}
---
+++ SodaMachineLibrary/ISodaMachineLogic.cs
@@ -0,0 +1,42 @@
+﻿using SodaMachineLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachineLibrary
+{
+    public interface ISodaMachineLogic
+    {
+        List<SodaModel> ListTypeOfSoda();
+
+        // Takes in an amount and returns the total deposited so far
+        decimal MoneyInserted(decimal moneytaryAmount);
+
+        // gets the overall price for sodas - there is no individual pricing
+        decimal GetSodaPrice();
+
+        // SodaModel (or null), List<Coin> Change, string ErrorMessage
+        (SodaModel soda, List<CoinModel> change, string errorMessage) RequestSoda(SodaModel soda);
+
+        void IssueFullRefund();
+
+        decimal GetMoneyInsertedTotal();
+
+        void AddtoInventory(List<SodaModel> sodas);
+
+        List<SodaModel> GetSodaInventory();
+
+        decimal EmptyMoneyFromMachine();
+
+        List<CoinModel> GetCoinInventory();
+
+        void AddToCoinInventory(List<CoinModel> coins);
+
+        decimal GetCurrentIncome();
+
+        decimal GetTotalIncome();
+
+    }
+}
